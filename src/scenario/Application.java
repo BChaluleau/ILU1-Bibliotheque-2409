bd9456554b6b246c3a0e130b68cfc6719873eabb
bch <bollombollom@yahoo.fr>
2024-09-24 08:28:46 +0200

    Commit initial

---
+++ src/scenario/Application.java
@@ -0,0 +1,12 @@
+package scenario;
+
+import domaine.Bibliotheque;
+
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("...");
+		Bibliotheque bibliotheque = new Bibliotheque();
+	}
+
+}
